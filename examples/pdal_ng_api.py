e69b981136070459ae7a057b27b08fc2b8806544
George Sakkis <george.sakkis@tiledb.com>
2021-09-14 12:22:51 +0300

    Add pdal.ng api usage and demo examples

---
+++ examples/pdal_ng_api.py
@@ -0,0 +1,59 @@
+import json
+
+from pdal.ng import Pipeline, Reader, Filter, Writer
+
+
+def create_pipeline() -> Pipeline:
+    readA = Reader(filename="A.las", spatialreference="EPSG:26916")
+    reproj = Filter.reprojection(in_srs="EPSG:26916", out_srs="EPSG:4326", tag="A2")
+    readB = Reader(filename="B.las", tag="B")
+    merge = Filter.merge(inputs=[reproj, readB])
+    write = Writer.gdal(filename="output.tif")
+    return readA | reproj | readB | merge | write
+
+
+def create_pipeline_one_line() -> Pipeline:
+    return (
+        Reader(filename="A.las", spatialreference="EPSG:26916")
+        | Filter.reprojection(in_srs="EPSG:26916", out_srs="EPSG:4326", tag="A2")
+        | Reader(filename="B.las", tag="B")
+        | Filter.merge(inputs=["A2", "B"])
+        | Writer.gdal(filename="output.tif")
+    )
+
+
+def create_pipeline_in_place() -> Pipeline:
+    p = Pipeline()
+    p |= Reader(filename="A.las", spatialreference="EPSG:26916")
+    p |= Filter.reprojection(in_srs="EPSG:26916", out_srs="EPSG:4326", tag="A2")
+    p |= Reader(filename="B.las", tag="B")
+    p |= Filter.merge(inputs=["A2", "B"])
+    p |= Writer.gdal(filename="output.tif")
+    return p
+
+
+def run_pipeline(name: str, pipeline: Pipeline) -> None:
+    pipeline.finalize()
+    print(f"* {name}.spec:", json.dumps(pipeline.spec, indent=4))
+    print(f"* {name}.process_points():")
+    print("Output:")
+    points = list(pipeline.process_points())
+    print("Points:")
+    for point in points:
+        print(f"\t{point}")
+    print(f"* {name}.process_chunks(2):")
+    print("Output:")
+    chunks = list(pipeline.process_chunks(2))
+    print("Chunks:")
+    for chunk in chunks:
+        print(f"\t{chunk}")
+    print()
+
+
+if __name__ == "__main__":
+    run_pipeline("p1", create_pipeline())
+    run_pipeline("p2", create_pipeline_one_line())
+    run_pipeline("p3", create_pipeline_in_place())
+
+    # p4 = Reader("A.las") | Writer("out1.las") | Reader("B.las") | Writer("out2.las")
+    # run_pipeline("p4", p4)
---
+++ examples/pdal_ng_demo.py
@@ -0,0 +1,73 @@
+from pprint import pprint
+from typing import Any, Iterable, Optional
+
+import numpy as np
+import pdal.ng
+
+
+class NumpyReader(pdal.ng.Reader):
+    def __init__(
+        self,
+        points: Iterable[pdal.ng.Point],
+        type: str = "readers.numpy",
+        **kwargs: Any
+    ):
+        super().__init__(points=points, type=type, **kwargs)
+
+    def process_points(
+        self, *point_streams: pdal.ng.PointStream
+    ) -> pdal.ng.PointStream:
+        return iter(self.points)
+
+
+class EvenFilter(pdal.ng.Filter):
+    def __init__(self, dim_idx: int, type: str = "filters.even", **kwargs: Any):
+        super().__init__(dim_idx=dim_idx, type=type, **kwargs)
+
+    def _filter_point(self, point: pdal.ng.Point) -> Optional[pdal.ng.Point]:
+        return point if point[self.dim_idx] % 2 == 0 else None
+
+
+class NegateFilter(pdal.ng.Filter):
+    def __init__(self, type: str = "filters.negate", **kwargs: Any):
+        super().__init__(type=type, **kwargs)
+
+    def _filter_point(self, point: pdal.ng.Point) -> Optional[pdal.ng.Point]:
+        return -point
+
+
+class StdoutWriter(pdal.ng.Writer):
+    def __init__(self, type: str = "writers.stdout", **kwargs: Any):
+        super().__init__(type=type, **kwargs)
+
+    def _write_point(self, point: pdal.ng.Point) -> None:
+        print(point)
+
+
+if __name__ == "__main__":
+    points = np.array([(i, 2 * i, 3 * i) for i in range(20)], dtype=np.float32)
+
+    print("========================== Input points ==========================")
+    pprint(points)
+    print()
+
+    pipeline = NumpyReader(points) | EvenFilter(dim_idx=2) | NegateFilter()
+    print("========================== Pipeline spec ==========================")
+    pprint(pipeline.spec)
+    print()
+
+    print("========================== Output points ==========================")
+    for i, point in enumerate(pipeline):
+        print(i, point)
+    print()
+
+    print("========================== Output point chunks ==========================")
+    for i, chunk in enumerate(pipeline.process_chunks(3)):
+        print(f"chunk {i}: {chunk.__class__.__name__}, {len(chunk)} points")
+        for point in chunk:
+            print("\t", point)
+    print()
+
+    print("========================== Pipe output to stdout ==========================")
+    for point in pipeline | StdoutWriter():
+        pass
