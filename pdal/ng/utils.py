f5afbd976cecaae67ee385d30cb5b0d38ae715a9
George Sakkis <george.sakkis@tiledb.com>
2021-09-16 10:16:04 +0300

    Rewrite rechunk_arrays, two orders of magnitude faster

--- pdal/ng/utils.py
+++ pdal/ng/utils.py
@@ -26,19 +26,33 @@ def rechunk_arrays(
 ) -> Iterator[NDArray[Scalar]]:
     """Rechunk an iterable of *arrays* into arrays of length *n*
 
-    >>> list(rechunk_arrays(chunk_array(np.arange(1, 11), 3), 4))
+    >>> list(rechunk_arrays(chunked(range(1, 11), 3), 4))
     [array([1, 2, 3, 4]), array([5, 6, 7, 8]), array([ 9, 10])]
 
     The last yielded array will have fewer than *n* elements if the sum of lengths
     of *arrays* is not divisible by *n*.
     """
-    # TODO: optimize this
-    buffer: List[Scalar] = []
-    for array in arrays:
-        buffer.extend(array)
-        if len(buffer) >= n:
-            yield np.array(buffer[:n])
-            del buffer[:n]
-    while buffer:
-        yield np.array(buffer[:n])
-        del buffer[:n]
+    pending: List[NDArray[Scalar]] = []
+    remaining_len = n
+    iter_arrays = iter(arrays)
+    array = next(iter_arrays, None)
+    while array is not None:
+        assert remaining_len > 0
+        array_len = len(array)
+        if array_len < remaining_len:
+            if array_len > 0:
+                pending.append(array)
+                remaining_len -= array_len
+            array = next(iter_arrays, None)
+        else:
+            # concatenate all pending arrays and a slice of the current array
+            pending.append(array[:remaining_len])
+            yield np.concatenate(pending) if len(pending) > 1 else pending[0]
+            # set the remaining slice of the current array as the next array
+            # to be processed and reset the pending and remaining_len
+            array = array[remaining_len:]
+            pending.clear()
+            remaining_len = n
+
+    if pending:
+        yield np.concatenate(pending) if len(pending) > 1 else pending[0]
